855814e77c296cf76160e3839f8bfbd86576ccc1
neverbeenthisweeb <rf.ramadhana@gmail.com>
2020-10-25 20:55:38 +0700

    Implement Blade layout

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        @yield('content')
+        <footer>
+            Copyright 2020 Larapizza
+        </footer>
+    </body>
+</html>
--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,27 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <h1>Pizzas</h1>
-        <p>WML with this Laravel OwO.</p>
-
-        <!-- @for($i = 0; $i < 5; $i++)
-            <p>The value of i is {{ $i }}</p>
-        @endfor -->
-
-        <!-- @for($i = 0; $i < count($pizzas); $i++)
-          <p>{{ $pizzas[$i]['type'] }}</p>
-        @endfor -->
-
-        @foreach($pizzas as $pizza)
-        <div>
-            {{ $loop->index }} {{ $pizza['type'] }} - {{ $pizza['base'] }}
-            @if($loop->first)
-                <span>- First in the loop!</span>
-            @endif
-            @if($loop->last)
-                <span>- Last in the loop!</span>
-            @endif
-        </div>
-        @endforeach
-    </body>
-</html>
+@extends('layouts.layout')
+@section('content')
+<h1>Pizzas</h1>
+<p>WML with this Laravel OwO.</p>
+@foreach($pizzas as $pizza)
+<div>
+    {{ $loop->index }} {{ $pizza['type'] }} - {{ $pizza['base'] }}
+    @if($loop->first)
+        <span>- First in the loop!</span>
+    @endif
+    @if($loop->last)
+        <span>- Last in the loop!</span>
+    @endif
+</div>
+@endforeach
+@endsection
