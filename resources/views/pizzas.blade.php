805083759cf06396d3f0c11426971103c7f56517
neverbeenthisweeb <rf.ramadhana@gmail.com>
2020-10-25 09:19:36 +0700

    Update pizzas.blade.php
    Implement Blade's if, unless and php features.

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -6,5 +6,22 @@
       <p>Type: {{ $type }}</p>
       <p>Base: {{ $base }}</p>
       <p>Price: ${{ $price }}</p>
+
+      @if($price > 15)
+        <p>This pizza is expensive!</p>
+      @elseif($price < 5)
+        <p>This pizza is affordable.</p>
+      @else
+        <p>This pizza is so so.</p>
+      @endif
+
+      @unless($base == 'Beef')
+        <p>You don't have a Beef Pizza!</p>
+      @endunless
+
+      @php
+        $name = 'Rafi';
+        echo $name;
+      @endphp
    </body>
 </html>
