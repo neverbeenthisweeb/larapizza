2fede0d12b247a2a89a6dced7095433a435a9d40
neverbeenthisweeb <rf.ramadhana@gmail.com>
2020-10-29 20:23:19 +0700

    Add logic to save record on /pizzas POST endpoint

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -27,9 +27,11 @@ class PizzaController extends Controller
     }
 
     public function store() {
-        error_log(request('name'));
-        error_log(request('type'));
-        error_log(request('base'));
-        return redirect('/');
+        $pizza = new Pizza();
+        $pizza->name = request('name');
+        $pizza->type = request('type');
+        $pizza->base = request('base');
+        $pizza->save();
+        return redirect('/')->with('message', 'Thanks for your order!');
     }
 }
